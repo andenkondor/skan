1b7c0bff7816afe8cd540f02a7038e0ca1d0671c
Timo Rolfsmeier <timo.rolfsmeier@device-insight.com>
2025-09-11 10:01:53 +0200

    feat: open file in nvim with skan staying open

--- skan.mjs
+++ skan.mjs
@@ -280,6 +280,7 @@ async function main() {
           "start,change:transform:(skan --internal-transform)",
           `ctrl-n:change-nth(${NTH.FILE_NAME}|${NTH.CODE_LINE}|)`,
           `ctrl-s:execute(idea --line {${NTH.LINE_NUMBER}} {${NTH.FILE_NAME}})`,
+          `alt-enter:execute(nvim {${NTH.FILE_NAME}} +{${NTH.LINE_NUMBER}} )`,
         ].flatMap((s) => ["--bind", s]),
 
         // colors
